97f28fc01d927a7711d600b365aa20da0bd3f3c6
mcontreras222 <142931221+mcontreras222@users.noreply.github.com>
2025-10-31 13:39:59 -0400

    final

--- stats.py
+++ stats.py
@@ -1,5 +1,25 @@
 import matplotlib.pyplot as plt
 
+def avg(ls):
+    sum = 0
+    for num in ls:
+        sum += num
+    return sum/len(ls)
+
+def aboveAvg(ls, avg):
+    above = 0
+    for num in ls:
+        if num > avg:
+            above += 1
+    return (above/len(ls))*100
+
+def median(ls):
+    ls.sort()
+    if len(ls)%2 == 0:
+        return (ls[len(ls)//2]+ls[(len(ls)//2)-1])/2
+    else:
+        return ls[len(ls)//2]
+
 fh = open("StudentExercise.csv")
 
 hours = []
@@ -13,4 +33,18 @@ for line in fh:
 
 plt.hist(hours, bins=10)
 
+average = avg(hours)
+median = median(hours)
+
+print("average:",average)
+
+print("percentage of students above average:", aboveAvg(hours,average), "%")
+
+print("median:", median)
+
 plt.show()
+
+
+
+
+
